15a2664d85a21f22c99c3136b4559bd9c33260a5
PIDA <webmaster@pida-ai.com>
2025-12-13 13:59:46 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -55,6 +55,7 @@ except:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://pida-ai-v20--.*\.web\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
